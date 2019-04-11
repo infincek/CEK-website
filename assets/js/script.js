e2d387c8a3f15a0161253d590168106108a8f4eb
Akshay <akshay48884@gmail.com>
2019-04-11 21:51:21 +0530

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,4 +10,23 @@ $(document).ready(function(){
     	coverTrigger: false,
     	constrainWidth: false
     });
+    $("#mobile a[data-target]").each(function(){
+        var li = $(this).parent();
+        var target = $(this).attr("data-target");
+        li.append($("#"+target)[0]);
+        $(this).removeAttr("data-target");
+        $(this).click(function(e){
+            e.preventDefault();
+            li.find(".dropdown-content").slideToggle(150);
+        })
+    })
+    var navTop = $("nav").offset().top;
+    $(window).scroll(function(){
+        console.log(navTop,$(this).scrollTop())
+        if($(this).scrollTop() > navTop){
+            $("nav").addClass("fixed");
+        }else{
+            $("nav").removeClass("fixed");
+        }
+    })
 });
