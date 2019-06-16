552aa9d9d93cca61095cbd2ec42c7f3f3875082f
Samanyu Neelson <samanyuneel@gmail.com>
2019-06-17 00:52:09 +0530

    small ajustments to file arrangement

--- index.php
+++ index.php
@@ -134,7 +134,7 @@ include "head.php";
 						<div class="item">
 							<div class="row">
 								<div class="col s12 m6 l5">
-									<img src="assets/img/infincek.png" alt="Campus Life"/>
+									<img src="assets/img/infincek.jpg" alt="Campus Life"/>
 								</div>
 								<div class="col s12 m6 l7">
 									<h3>InfinCEK</h3>
